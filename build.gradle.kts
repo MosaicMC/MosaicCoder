1da035a5d5cbf79bff12ce7a5dcb3881da150d6f
justfoxx <justfoxxo@outlook.com>
2023-08-06 10:13:58 +0200

    Binary clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,12 @@ tasks {
 
     withType<KotlinCompile>().configureEach {
         kotlinOptions.jvmTarget = "17"
-        kotlinOptions.freeCompilerArgs += "-Xlambdas=indy"
+        kotlinOptions.freeCompilerArgs += listOf(
+            "-Xlambdas=indy",
+            "-Xno-param-assertions",
+            "-Xno-call-assertions",
+            "-Xno-receiver-assertions",
+        )
     }
 
     register<Jar>("dokkaHtmlJar") {
